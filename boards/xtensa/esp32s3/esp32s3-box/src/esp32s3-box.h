1c231556fb69f55fd9e8c2e835b67609ddc88fd0
jiayadong <jiayadong@xiaomi.com>
2025-06-30 17:07:17 +0800

    chenge config

--- boards/xtensa/esp32s3/esp32s3-box/src/esp32s3-box.h
+++ boards/xtensa/esp32s3/esp32s3-box/src/esp32s3-box.h
@@ -183,5 +183,30 @@ void board_lcd_uninitialize(void);
 int board_touchscreen_initialize(void);
 #endif
 
+/****************************************************************************
+ * Name: esp32s3_es8311_initialize
+ *
+ * Description:
+ *   This function is called by platform-specific, setup logic to configure
+ *   and register the ES8311 device.  This function will register the driver
+ *   as /dev/audio/pcm[x] where x is determined by the I2S port number.
+ *
+ * Input Parameters:
+ *   i2c_port  - The I2C port used for the device
+ *   i2c_addr  - The I2C address used by the device
+ *   i2c_freq  - The I2C frequency used for the device
+ *   i2s_port  - The I2S port used for the device
+ *
+ * Returned Value:
+ *   Zero is returned on success.  Otherwise, a negated errno value is
+ *   returned to indicate the nature of the failure.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_AUDIO_ES8311
+int esp32s3_es8311_initialize(int i2c_port, uint8_t i2c_addr, int i2c_freq,
+                            int i2s_port);
+#endif
+
 #endif /* __ASSEMBLY__ */
 #endif /* __BOARDS_XTENSA_ESP32S3_ESP32S3_BOX_SRC_ESP32S3_BOX_H */
