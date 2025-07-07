a0059d33a79453375eb4916467106528bf5dd7f0
jiayadong <jiayadong@xiaomi.com>
2025-07-07 16:23:46 +0800

    add bmi088 sensor base

---
+++ drivers/sensors/bmi088_base.h
@@ -0,0 +1,71 @@
+/****************************************************************************
+ * drivers/sensors/bmi088_base.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __INCLUDE_NUTTX_SENSORS_BMI088_BASE_H
+#define __INCLUDE_NUTTX_SENSORS_BMI088_BASE_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <debug.h>
+
+#include <nuttx/arch.h>
+#include <nuttx/kmalloc.h>
+#include <nuttx/fs/fs.h>
+#include <nuttx/sensors/bmi088.h>
+#  include <nuttx/spi/spi.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+struct bmi088_dev_s
+{
+#ifdef /* CONFIG_SENSORS_BMI088_SPI */
+  FAR struct spi_dev_s    *spi;  /* SPI interface */
+#endif
+};
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+extern const uint8_t g_bmi088_config_file[];
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+
+
+#endif  /* __INCLUDE_NUTTX_SENSORS_BMI088_BASE_H */
